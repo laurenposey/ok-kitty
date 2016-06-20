bfe69c3fefe505c1d54dd0903864c15e470e012e
Charles Ewel and Lauren Posey <charles@>
2016-06-20 11:31:07 -0700

    updates javascript to create user correctly

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,26 @@ User.prototype.fullName = function() {
   return fullName;
 }
 
+User.prototype.haveKidsWritten = function() {
+  if(this.haveKids) {
+    return "have kids in the home";
+  } else {
+    return "do not have kids in the home";
+  }
+}
+User.prototype.haveCatsWritten = function() {
+  if(this.haveCats===0) {
+    return "do not have other cats at the moment";
+  } else if(this.haveCats===1) {
+    return "have one other cat";
+  } else {
+    return "have multiple cats";
+  }
+}
+
 $(document).ready(function(){
-  $("#adopter-input").submit(function(){
+  $("#adopter-input").submit(function(event){
+    event.preventDefault();
     userFirstName=$("input#firstName").val();
     userLastName=$("input#lastName").val();
     userDOB=$("input#userBirthDate").val();
@@ -23,8 +41,13 @@ $(document).ready(function(){
     userCatAge=parseInt($("input#userCatAge").val());
     userCatHealth=$("#userCatHealth").val();
 
-    var User1=new User(userFirstName, userLastName, userHaveKids, userHaveKids, userDOB, userIsIndoor);
-
-    $("profile-display").text("Hi "+User1.fullName()+", our records show you")
+    var User1=new User(userFirstName, userLastName, userHaveCats, userHaveKids, userDOB, userIsIndoor);
+    // $("profile-display").show();
+    // $(".user-full-name")=User1.fullName();
+    // $(".user-has-kids")=User1.haveKidsWritten();
+    // $(".user-has-cats")=User1.haveCatsWritten();
+    // $(".user-DOB")=User1.userDOB;
+    // $(".cat-indoor")=User1.isIndoor;
+    console.log(User1);
   });
 });
