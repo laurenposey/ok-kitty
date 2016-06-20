67595ec53da99095501064e64ad474ea9aeffd12
Charles Ewel and Lauren Posey <lauren@>
2016-06-20 13:23:13 -0700

    generate kitties with random names and attributes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
+var kitties = [];
+var catnames = ["Chairman Meow", "Lulu", "June", "Penny", "Queen Gwendolyn", "Hazel", "Huxley", "Lady Meowingtons", "Meow Ming", "Todd", "Chad", "Steve", "Keith", "Mrs Little Piddles"];
+function kittyCat() {
+  for(var i=0; i<=1; i++) {
+    for(var j=0; j<=1; j++) {
+      for(var k=0; k<=1; k++) {
+        var name = catnames[i+(2*j)+(4*k)]
+        var cats=true;
+        var kids=true;
+        var indoor=true;
+        if(i===0) {
+          cats=false;
+        }
+        if(j===0) {
+          kids=false;
+        }
+        if(k===0) {
+          indoor=false;
+        }
+        var Kitten = new Kitty(name, cats, kids, indoor, "01011990");
+        kitties.push(Kitten);
+      }
+    }
+  }
+}
+
 function User(firstName, lastName, haveCats, haveKids, userDOB, isIndoor) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -5,6 +31,14 @@ function User(firstName, lastName, haveCats, haveKids, userDOB, isIndoor) {
   this.haveKids = haveKids;
   this.userDOB = userDOB;
   this.isIndoor = isIndoor;
+}
+
+function Kitty(kittyName, isOnlyKitty, isOnlyChild, isIndoor, kittyDOB) {
+  this.kittyName = kittyName;
+  this.isOnlyKitty = isOnlyKitty;
+  this.isOnlyChild = isOnlyChild;
+  this.isIndoor = isIndoor;
+  this.kittyDOB = kittyDOB;
 }
 
 User.prototype.fullName = function() {
@@ -32,6 +66,7 @@ User.prototype.haveCatsWritten = function() {
 $(document).ready(function(){
   $("#adopter-input").submit(function(event){
     event.preventDefault();
+    kittyCat();
     userFirstName=$("input#firstName").val();
     userLastName=$("input#lastName").val();
     userDOB=$("input#userBirthDate").val();
@@ -42,12 +77,12 @@ $(document).ready(function(){
     userCatHealth=$("#userCatHealth").val();
 
     var User1=new User(userFirstName, userLastName, userHaveCats, userHaveKids, userDOB, userIsIndoor);
-    // $("profile-display").show();
+    // $("#profile-display").show();
     // $(".user-full-name")=User1.fullName();
     // $(".user-has-kids")=User1.haveKidsWritten();
     // $(".user-has-cats")=User1.haveCatsWritten();
     // $(".user-DOB")=User1.userDOB;
     // $(".cat-indoor")=User1.isIndoor;
-    console.log(User1);
+    kitties.forEach()
   });
 });
